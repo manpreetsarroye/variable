c2d3aa88c3356aa30fbb2459ceaf2574f900db1a
Manpreet Kaur <102292106+manpreetsarroye@users.noreply.github.com>
2022-07-02 16:08:13 +0530

    Add files via upload

---
+++ meraki.js
@@ -0,0 +1,22 @@
+const { question } = require("readline-sync");
+
+// question1
+var num2=6;
+var num1=num2;
+console.log(num1)
+
+//  question3
+
+var a;
+var b;
+var c;
+console.log(a,b,c);
+console.log(a+2);
+
+// question5
+var num1=2.3;
+var num2=5.2;
+console.log(num1*num2)
+
+question
+
